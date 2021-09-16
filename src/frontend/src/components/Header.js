a6b9e980268bdc67dd04255fe04580356e30960a
jiteshsrivastava <jiteshsrilko@gmail.com>
2021-09-16 21:57:24 +0530

    first comit

---
+++ src/frontend/src/components/Header.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Nav, Navbar, Container, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
+import logo from './logo.png';
+
+export default function header() {
+    return (
+
+        <Navbar collapseOnSelect expand="lg" >
+            <Container>
+                <Navbar.Brand>
+                    <a href="#">
+                        <img src={logo} />
+                    </a>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#features">About Us</Nav.Link>
+                        <Nav.Link href="#pricing">Notification</Nav.Link>
+                        <Nav.Link href="#pricing">Facilities</Nav.Link>
+                        <Nav.Link href="#pricing">Admission</Nav.Link>
+                        <Nav.Link href="#pricing">Career</Nav.Link>
+                        <NavDropdown title="Student Zone" id="collasible-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Photo Gallery</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Vedio Gallery</NavDropdown.Item>
+                        </NavDropdown>
+                        <NavDropdown title="Gallery" id="collasible-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Photo Gallery</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Vedio Gallery</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                    <Nav>
+                        <Nav.Link href="#deets">Contact us</Nav.Link>
+                        <Nav.Link eventKey={2} href="#memes">
+                            Get A Quote
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+
+    )
+}
