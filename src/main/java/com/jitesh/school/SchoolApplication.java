7b1803aff5c9e97dd070bf990d03c9c811bed6ad
jiteshsrivastava <jiteshsrilko@gmail.com>
2021-09-16 22:54:39 +0530

    dummy commit

--- src/main/java/com/jitesh/school/SchoolApplication.java
+++ src/main/java/com/jitesh/school/SchoolApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SchoolApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Spring boot loading..");
 		SpringApplication.run(SchoolApplication.class, args);
 	}
 
