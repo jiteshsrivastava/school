dc7a6dabfafd9bcc713327a718e0c5e2836bf6eb
jiteshsrivastava <jiteshsrilko@gmail.com>
2021-09-25 00:00:48 +0530

    aboutus added

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,14 +1,27 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from "./components/Header";
-import ImgSlider from './components/imgSlider/ImgSlider';
 
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import ImgSlider from "./components/imgSlider/ImgSlider";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import About from "./components/aboutUs/About";
 
 function App() {
   return (
-    <div className="App" >    
-     <Header/>
-     <ImgSlider/>
+    <div className="App">
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <ImgSlider />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
